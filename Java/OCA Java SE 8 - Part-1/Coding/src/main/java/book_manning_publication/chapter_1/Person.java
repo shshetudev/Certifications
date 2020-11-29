28203cbbeac110f79e7c8af287a5b4f412229b84
shshetudev <shshetudev@gmail.com>
2020-11-29 08:37:57 +0600

    starting chapter-2

---
+++ Java/OCA Java SE 8 - Part-1/Coding/src/main/java/book_manning_publication/chapter_1/Person.java
@@ -0,0 +1,12 @@
+package book_manning_publication.chapter_1;
+
+public class Person {
+    public static void main(String[] args) {
+        final StringBuilder name = new StringBuilder("Shetu");
+        name.append(" Shahariar");
+        System.out.println(name);
+        // But it will not compile
+//        name = new StringBuilder();
+
+    }
+}
