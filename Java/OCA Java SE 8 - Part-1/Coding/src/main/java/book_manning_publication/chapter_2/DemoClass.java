b80e3520b4954f6c86901b3303141c0031729289
shetu <shahariar.hossen@datasoft-bd.com>
2020-11-29 18:01:47 +0600

    chapter-2

---
+++ Java/OCA Java SE 8 - Part-1/Coding/src/main/java/book_manning_publication/chapter_2/DemoClass.java
@@ -0,0 +1,12 @@
+package book_manning_publication.chapter_2;
+
+public class DemoClass {
+    public static void main(String[] args) {
+//        int a , b, c;
+//        a = b = c = 10;
+//        System.out.println("Hello world");
+        boolean b1 , b2, b3;
+        b1 = b2 = b3 = true;
+        System.out.println("b1: " + b1 + " b2: "+ b2+ " b3: " + b3);
+    }
+}
